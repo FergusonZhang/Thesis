f6c8afc6f3eaa857fbb78ae84e59cff956310ce3
zhangz16 <zhangz16@rose-hulman.edu>
2021-05-06 22:13:28 -0400

    Avoid using math and PyVCF at the same time

--- Computational_analysis.py
+++ Computational_analysis.py
@@ -1,5 +1,4 @@
 # This file contains the calculation of nucleotide diversity.
-import math
 import numpy as np
 import warnings
 from Parse_the_input import *
@@ -14,12 +13,25 @@ def pairwise_difference(first_sequence, second_sequence):
     return count
 
 
+def comb(n, k):
+    if 0 <= k <= n:
+        ntok = 1
+        ktok = 1
+        for t in range(1, min(k, n - k) + 1):
+            ntok *= n
+            ktok *= t
+            n -= 1
+        return ntok // ktok
+    else:
+        return 0
+
+
 def nucleotide_diversity(sequences):
     pi_value = 0
     for first_sequence in sequences:
         for second_sequence in sequences:
             pi_value += pairwise_difference(first_sequence, second_sequence)
-    pi_value = pi_value/(2*math.comb(len(sequences), 2))
+    pi_value = pi_value/(2*comb(len(sequences), 2))
     return pi_value
 
 
@@ -43,7 +55,7 @@ def tajima_test(sequences):
     e_2 = c_2/(a_1**2 + a_2)
     s = get_segregating_sites(sequences)
     k = nucleotide_diversity(sequences)
-    tajima_score = (k - s/a_1)/math.sqrt(e_1*s + e_2*s*(s - 1))
+    tajima_score = (k - s/a_1)/np.sqrt(e_1*s + e_2*s*(s - 1))
     return tajima_score
 
 
