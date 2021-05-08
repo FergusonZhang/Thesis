cb10ea431758e896dbd81a03e37a76f3224ec2ba
zhangz16 <zhangz16@rose-hulman.edu>
2021-05-08 05:00:44 -0400

    A bug regarding to segregating site fixed.

--- Computational_analysis.py
+++ Computational_analysis.py
@@ -6,14 +6,13 @@ import pprint as ppt
 warnings.filterwarnings("ignore")
 
 
-# Handle different inputs based on their formats
-def file_parser(filename):
-    names = filename.split(".")
-    file_type = names[1]
+# Read sequences from different files
+def read_sequences(file_name):
+    name = file_name.split(".")
+    file_type = name[1]
     if file_type == "fas":
-        print("This is a FASTA file.")
         sequences = []
-        with open(filename) as f:
+        with open(file_name) as f:
             current_sequence = ""
             for line in f:
                 if line[0] != ">":
@@ -26,7 +25,6 @@ def file_parser(filename):
                 sequences.append(current_sequence)
         return sequences
     elif file_type == "vcf":
-        print("This is a VCF file.")
         # TODO: handle the VCF file.
         return
     else:
@@ -34,8 +32,8 @@ def file_parser(filename):
         return
 
 
-# Count single nucleotide polymorphisms between two sequences.
-def pairwise_differences(first_sequence, second_sequence):
+# Count single nucleotide polymorphism between two sequences.
+def pairwise_difference(first_sequence, second_sequence):
     return np.sum(x != y for x, y in zip(first_sequence, second_sequence))\
             + abs(len(first_sequence) - len(second_sequence))
 
@@ -54,25 +52,24 @@ def comb(n, k):
         return 0
 
 
-# Calculate nucleotide diversity using pairwise differences.
+# Calculate nucleotide diversity using pairwise difference.
 def nucleotide_diversity(sequences):
     pi_value = 0
     for first_sequence in sequences:
         for second_sequence in sequences:
-            pi_value += pairwise_differences(first_sequence, second_sequence)
+            pi_value += pairwise_difference(first_sequence, second_sequence)
     return pi_value/(2*comb(len(sequences), 2))
 
 
-# Count the number of segregating sites.
-def segregating_sites(sequences):
-    result_list = []
+# Count the number of segregating site.
+def segregating_site(sequences):
     current_list = []
+    result_list = []
     for first_sequence in sequences:
         for second_sequence in sequences:
             current_list = [index for index in range(len(first_sequence)) if
                             first_sequence[index] != second_sequence[index]]
-        result_list = sorted(np.unique(current_list + result_list))
-        current_list = []
+            result_list = sorted(np.unique(current_list + result_list))
     return len(result_list)
 
 
@@ -82,27 +79,26 @@ def tajima_test(sequences):
     a_1 = 0
     a_2 = 0
     for number in range(1, n):
-        a_1 += 1 / number
-        a_2 += 1 / number**2
+        a_1 += 1/number
+        a_2 += 1/number**2
     b_1 = (n + 1)/(3*(n - 1))
     b_2 = 2*(n**2 + n + 3)/(9*n*(n - 1))
     c_1 = b_1 - 1/a_1
     c_2 = b_2 - (n + 2)/(a_1*n) + a_2/a_1**2
     e_1 = c_1/a_1
     e_2 = c_2/(a_1**2 + a_2)
-    s = segregating_sites(sequences)
+    s = segregating_site(sequences)
     if s == 0:
         return 0
     k = nucleotide_diversity(sequences)
-    avg_length = 0
+    total_length = 0
     for sequence in sequences:
-        avg_length += len(sequence)
-    avg_length = avg_length/n
-    return (k - s/a_1)/((np.sqrt(e_1*s + e_2*s*(s - 1)))*avg_length)
+        total_length += len(sequence)
+    return (k - s/a_1)/((np.sqrt(e_1*s + e_2*s*(s - 1)))*total_length/n)
 
 
-# Parse sequences into pieces with fixed size
-def parse_to_pieces(sequences, window_size):
+# Parse sequence into pieces with fixed length
+def parse_into_pieces(sequences, window_size):
     pieces = []
     for index_1, sequence in enumerate(sequences):
         num = len(sequence)//window_size
@@ -118,27 +114,26 @@ def parse_to_pieces(sequences, window_size):
 
 # The main function.
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Genome reader.')
-    parser.add_argument(dest='file_name', help='Please input the genome filename.')
+    parser = argparse.ArgumentParser(description='Balancing selection detector.')
+    parser.add_argument(dest='file_name', help='Please input the filename.')
     args = parser.parse_args()
     print("The input file is: " + str(args.file_name))
-    Sequences = file_parser(args.file_name)
+    Sequences = read_sequences(args.file_name)
     print("The sequences are: ")
     ppt.pprint(Sequences)
     Pi_value = nucleotide_diversity(Sequences)
     print("The nucleotide diversity is: " + str(Pi_value))
     Tajima_D = tajima_test(Sequences)
     print("The Tajima's D is: " + str(Tajima_D))
-    Parsed_sequences = parse_to_pieces(Sequences, 5)
+
+    Parsed_sequences = parse_into_pieces(Sequences, 5)
     print("The parsed sequences are: ")
     ppt.pprint(Parsed_sequences)
-
-    # Todo: More implements here.
     Bp_position = []
     Tajima_scores = []
     for i in range(len(Parsed_sequences[0])):
         Bp_position.append(i*5 + 3)
         Tajima_scores.append(tajima_test(Parsed_sequences[i]))
-    print("The Tajima's D vs. position is: ")
+    print("The score vs. position is: ")
     ppt.pprint(Bp_position)
     ppt.pprint(Tajima_scores)
