8b42917c0210aef59f2e009f6f6059f29ad2b41e
zhangz16 <zhangz16@rose-hulman.edu>
2021-09-19 19:56:52 -0400

    OS and Pickle package imported, intermediate files saved in directory.

--- Computational_analysis.py
+++ Computational_analysis.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     print("The Tajima's D is: " + str(Tajima_D))
 
     Parsed_sequences = parse_into_pieces(Sequences, args.window_size)
-    # print("The input window size is: " + str(args.window_size))
+    print("The input window size is: " + str(args.window_size))
     # print("The parsed sequences are: ")
     # ppt.pprint(Parsed_sequences)
 
