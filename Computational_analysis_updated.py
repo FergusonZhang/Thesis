0d5a26e9b869a7b5bd3c6f9f36c7035a2a0d2552
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-05 00:10:26 -0400

    Try to increase the code efficiency.

--- Computational_analysis_updated.py
+++ Computational_analysis_updated.py
@@ -92,6 +92,7 @@ if __name__ == "__main__":
     print("The sample size is: " + str(Sample_size))
     print("The number of polymorphic base pair is: " + str(len(Base_pair_positions)))
     print("The true length of the genome is: " + str(Base_pair_positions[-1]))
+    ppt.pprint(Allele_frequencies)
 
     Pi_value = get_nucleotide_diversity(Allele_frequencies, Sample_size)
     print("The nucleotide diversity is: " + str(Pi_value))
