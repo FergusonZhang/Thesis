113f28e9f56f8507f0c775ae2bd097167318876d
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-05 01:26:22 -0400

    Try to increase the code efficiency.

--- Computational_analysis_updated.py
+++ Computational_analysis_updated.py
@@ -123,6 +123,10 @@ if __name__ == "__main__":
     # print("The length of the last element is: " + str(len(Parsed_frequencies[-1])))
 
     Tajima_Ds = analyze_parsed_frequency(Parsed_frequencies, args.window_size)
+    print("The length of the Tajima_Ds is: " + str(len(Tajima_Ds)))
+    print("The length of the Parsed_positions is: " + str(len(Parsed_positions)))
+    ppt.pprint(Tajima_Ds)
+    ppt.pprint(Parsed_positions)
     plt.plot(Parsed_positions, Tajima_Ds, color='blue', linestyle='dashed', linewidth=1,
              marker='.', markerfacecolor='blue', markersize=5)
     plt.xlabel("Position")
