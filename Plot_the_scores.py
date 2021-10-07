00365dbe8d1dafee04e8ed08fe268d5989513b4d
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-07 09:40:18 -0400

    Plotting function added.

--- Plot_the_scores.py
+++ Plot_the_scores.py
@@ -9,12 +9,12 @@ if __name__ == '__main__':
     parser.add_argument(dest='positions_file_name', help='Please enter the base pair position filename.')
     parser.add_argument(dest='scores_file_name', help="Please enter the Tajima's D file name.")
     args = parser.parse_args()
-    print("The input position file is: " + str(args.positions_file_name))
-    print("The input score file is: " + str(args.scores_file_name))
+    print('The input position file is: ' + str(args.positions_file_name))
+    print('The input score file is: ' + str(args.scores_file_name))
 
     infile = open(args.positions_file_name, 'rb')
     Parsed_positions = pickle.load(infile)
-    infile = open(args.scores_file.name, 'rb')
+    infile = open(args.scores_file_name, 'rb')
     Tajimas_ds = pickle.load(infile)
     infile.close()
 
