e9bc51b7822e8bf4038dba9b27d2735e90d1b7d4
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-07 09:33:00 -0400

    Sequence parsing refined, Tajima's Ds and parsed positions saved as pickle files. Plotting function initiated.

--- Computational_analysis.py
+++ Computational_analysis.py
@@ -1,25 +1,22 @@
-# This program takes an VCF file and a window size as inputs
-# The outputs are a list of Tajima's Ds and a list of positions for the parsed sequence
-# It will also create a figure of Tajima's D vs. Position
+# This program will return a list of Tajima's D and a list of position for a given input VCF file with a window size
 import argparse
-import matplotlib.pyplot as plt
 import numpy as np
-import warnings
-import vcf
 import pickle
-warnings.filterwarnings("ignore")
+import vcf
+import warnings
+warnings.filterwarnings('ignore')
 
 
-# Get sample size, # of segregating site, base pair positions, and nucleotide diversities from the VCF file
+# Get sample size, # of segregating site, base pair positions, and nucleotide diversities
 def get_info(file_name):
     base_pair_positions = []
     nucleotide_diversities = []
     reader = vcf.Reader(open(file_name, 'r'))
-    sample_size = len(reader.samples)*2  # Assume each position has the same sample size
+    sample_size = len(reader.samples)*2
     for record in reader:
         base_pair_positions.append(record.POS)
         nucleotide_diversities.append(record.nucl_diversity)
-    segregating_sites = len(base_pair_positions)  # Assume each site in the data set is a polymorphic site
+    segregating_sites = len(base_pair_positions)
     return [sample_size, segregating_sites, base_pair_positions, nucleotide_diversities]
 
 
@@ -42,53 +39,46 @@ def prepare_tajimas_d(n):
 # Calculate the Tajima's D ( k is the nucleotide diversity and s is the # of segregating site)
 def get_tajimas_d(k, s, a_1, e_1, e_2):
     if (np.sqrt(e_1*s + e_2*s*(s - 1))) == 0:
-        return float("nan")
+        return float('nan')
     else:
         return (k - s/a_1)/np.sqrt(e_1*s + e_2*s*(s - 1))
 
 
-# Calculate the Tajima's Ds for parsed sequences as well as corresponding positions
+# Calculate the Tajima's Ds for parsed sequences as well as corresponding base pair positions
 def analyze_parsed_sequence(sample_size, segregating_sites, base_pair_positions, nucleotide_diversities, window_size):
     [a_1, e_1, e_2] = prepare_tajimas_d(sample_size)
-    tajima_ds = []
     parsed_positions = []
+    tajimas_ds = []
     num = segregating_sites//window_size
     for index in range(num):
         parsed_positions.append(np.average(base_pair_positions[index*window_size:(index + 1)*window_size]))
         k = np.sum(nucleotide_diversities[index*window_size:(index + 1)*window_size])
-        tajima_ds.append(get_tajimas_d(k, window_size, a_1, e_1, e_2))
+        tajimas_ds.append(get_tajimas_d(k, window_size, a_1, e_1, e_2))
     if segregating_sites % window_size != 0:
         parsed_positions.append(np.average(base_pair_positions[num*window_size:]))
         k = np.sum(nucleotide_diversities[num*window_size:])
-        tajima_ds.append(get_tajimas_d(k, segregating_sites % window_size, a_1, e_1, e_2))
-    return [parsed_positions, tajima_ds]
+        tajimas_ds.append(get_tajimas_d(k, segregating_sites % window_size, a_1, e_1, e_2))
+    return [parsed_positions, tajimas_ds]
 
 
 # The main function.
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Balancing selection detector.')
-    parser.add_argument(dest='file_name', help='Please enter the filename.')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Balancing selection analysis.')
+    parser.add_argument(dest='file_name', help='Please enter the VCF filename.')
     parser.add_argument(dest='window_size', help='Please enter the window size.', type=int)
     args = parser.parse_args()
-    print("The input file is: " + str(args.file_name))
-    print("The input window size is: " + str(args.window_size))
+    print('The input file is: ' + str(args.file_name))
+    print('The input window size is: ' + str(args.window_size))
 
     [Sample_size, Segregating_sites, Base_pair_positions, Nucleotide_diversities] = get_info(args.file_name)
-    print("The sample size is: " + str(Sample_size))
-    print("The number of segregating site is: " + str(Segregating_sites))
-    print("The true length of the genome is: " + str(Base_pair_positions[-1]))
+    print('The sample size is: ' + str(Sample_size))
+    print('The number of segregating site is: ' + str(Segregating_sites))
+    print('The true length of the scaffold is: ' + str(Base_pair_positions[-1]))
 
-    [Parsed_positions, Tajima_Ds] = analyze_parsed_sequence(
+    [Parsed_positions, Tajimas_ds] = analyze_parsed_sequence(
         Sample_size, Segregating_sites, Base_pair_positions, Nucleotide_diversities, args.window_size)
-    with open(f"{args.file_name}_{args.window_size}_scores.pkl", "wb") as t:
-        pickle.dump(Tajima_Ds, t)
-    with open(f"{args.file_name}_{args.window_size}_positions.pkl", "wb") as p:
+    with open(f'{args.file_name}_{args.window_size}_scores.pkl', 'wb') as t:
+        pickle.dump(Tajimas_ds, t)
+    with open(f'{args.file_name}_{args.window_size}_positions.pkl', 'wb') as p:
         pickle.dump(Parsed_positions, p)
-    print("The number of parsed fragment is: " + str(len(Tajima_Ds)))
-
-    # Plot the Tajima's D vs. Position
-    plt.plot(Parsed_positions, Tajima_Ds, Color="blue", linewidth=0.7)
-    plt.xlabel("Base Pair Position")
-    plt.ylabel("Tajima's D")
-    plt.title(f"{args.file_name} Balancing Selection Analysis")
-    plt.savefig(f"{args.file_name}_{args.window_size}_figure.png", dpi=200)
+    print('The number of parsed fragment is: ' + str(len(Tajimas_ds)))
---
+++ Plot_the_scores.py
@@ -0,0 +1,24 @@
+# This program will take the lists of Tajima's D and position to create a figure.
+import argparse
+import matplotlib.pyplot as plt
+import pickle
+
+# The main function.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tajima figure plotting.')
+    parser.add_argument(dest='positions_file_name', help='Please enter the base pair position filename.')
+    parser.add_argument(dest='scores_file_name', help="Please enter the Tajima's D file name.")
+    args = parser.parse_args()
+    print("The input position file is: " + str(args.positions_file_name))
+    print("The input score file is: " + str(args.scores_file_name))
+
+    infile = open(args.positions_file_name, 'rb')
+    Parsed_positions = pickle.load(infile)
+    infile = open(args.scores_file.name, 'rb')
+    Tajimas_ds = pickle.load(infile)
+    infile.close()
+
+    plt.plot(Parsed_positions, Tajimas_ds, Color='blue', linewidth=0.7)
+    plt.xlabel('Base Pair Position')
+    plt.ylabel("Tajima's D")
+    plt.savefig(f'{args.file_name}_{args.window_size}_figure.png', dpi=200)
