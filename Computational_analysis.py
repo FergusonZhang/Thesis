7e4267eabd56c59d3ea40348b985b43e7f609d7d
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-06 20:35:27 -0400

    Sequence parsing refined, Tajima's Ds and parsed positions saved as pickle files.

--- Computational_analysis.py
+++ Computational_analysis.py
@@ -87,7 +87,8 @@ if __name__ == "__main__":
     print("The number of parsed fragment is: " + str(len(Tajima_Ds)))
 
     plt.plot(Parsed_positions, Tajima_Ds)
+    plt.rcParams["figure.figsize"] = (20, 3)
     plt.xlabel("Base Pair Position")
     plt.ylabel("Tajima's D")
     plt.title(f"{args.file_name} Balancing Selection Analysis")
-    plt.savefig(f"{args.file_name}_{args.window_size}_figure.png")
+    plt.savefig(f"{args.file_name}_{args.window_size}_figure.png", dpi=100)
