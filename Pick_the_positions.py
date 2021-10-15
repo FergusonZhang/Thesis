13f410762735bb09e150c5a2a9e7e2313a3b2931
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-15 15:09:16 -0400

    Peak candidates elected.

--- Pick_the_positions.py
+++ Pick_the_positions.py
@@ -10,15 +10,12 @@ if __name__ == '__main__':
     parser.add_argument(dest='score_file_name', help='Please enter the score file name.')
     parser.add_argument(dest='expected_size', help='Please enter the expected size.', type=int)
     args = parser.parse_args()
-    print('The input base pair position file is: ' + str(args.position_file_name))
-    print("The input Tajima's D file is: " + str(args.score_file_name))
 
     infile = open(args.position_file_name, 'rb')
     Parsed_positions = pickle.load(infile)
     infile = open(args.score_file_name, 'rb')
     Tajimas_ds = pickle.load(infile)
     infile.close()
-
     index = np.argsort(Tajimas_ds)
     index = index[::-1]
     Sorted_positions = np.array(Parsed_positions)[index]
