cdcfe3ee74c4f4ce9a18776db6689c674cffba60
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-21 01:37:03 -0400

    Plotting function further modified so that candidate sites will be marked red.

--- Plot_the_scores.py
+++ Plot_the_scores.py
@@ -1,6 +1,7 @@
 # This program will take a list of Tajima's D and a list of position to create a figure
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 import pickle
 
 # The main function.
@@ -8,6 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Figure plotting.')
     parser.add_argument(dest='position_file_name', help='Please enter the position file name.')
     parser.add_argument(dest='score_file_name', help='Please enter the score file name.')
+    parser.add_argument(dest='expected_size', help='Please enter the expected size.', type=int)
     args = parser.parse_args()
 
     infile = open(args.position_file_name, 'rb')
@@ -16,9 +18,17 @@ if __name__ == '__main__':
     Tajimas_ds = pickle.load(infile)
     infile.close()
 
-    # Figure plotting
+    index = np.argsort(Tajimas_ds)
+    index = index[::-1]
+    Sorted_positions = np.array(Parsed_positions)[index]
+    Sorted_scores = np.array(Tajimas_ds)[index]
+    Sorted_positions = Sorted_positions[0:args.expected_size]
+    Sorted_scores = Sorted_scores[0:args.expected_size]
+
     plt.figure(figsize=(40, 5))
     plt.plot(Parsed_positions, Tajimas_ds, Color='blue', linewidth=0.5)
+    plt.plot(Sorted_positions, Sorted_scores, 'ro', markersize=2)
+    plt.title('Balancing Selection Candidate sites')
     plt.xlabel('Base Pair Position')
     plt.ylabel("Tajima's D")
     plt.savefig(f'{args.score_file_name}_figure.png', dpi=500)
