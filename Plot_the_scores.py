01680edabd6334961fed2d8f6b662aedb8f2d248
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-27 21:21:28 -0400

    Plot the Tajima's D vs. Position with outliers marked red.

--- Plot_the_scores.py
+++ Plot_the_scores.py
@@ -1,34 +1,34 @@
 # This program will take a list of Tajima's D and a list of position to create a figure
 import argparse
 import matplotlib.pyplot as plt
-import numpy as np
 import pickle
 
 
 # The main function.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Figure plotting.')
-    parser.add_argument(dest='position_file_name', help='Please enter the position file name.')
-    parser.add_argument(dest='score_file_name', help='Please enter the score file name.')
-    parser.add_argument(dest='expected_size', help='Please enter the expected size.', type=int)
+    parser.add_argument(dest='window_size', help='Please enter the parsing window size.', type=int)
+    parser.add_argument(dest='cut_off', help='Please enter the cut off value.', type=int)
     args = parser.parse_args()
 
-    infile = open(args.position_file_name, 'rb')
-    Parsed_positions = pickle.load(infile)
-    infile = open(args.score_file_name, 'rb')
-    Tajimas_ds = pickle.load(infile)
-    infile.close()
-    index = np.argsort(Tajimas_ds)
-    index = index[::-1]
-    Sorted_positions = np.array(Parsed_positions)[index]
-    Sorted_scores = np.array(Tajimas_ds)[index]
-    Sorted_positions = Sorted_positions[0:args.expected_size]
-    Sorted_scores = Sorted_scores[0:args.expected_size]
+    Outlier_positions = []
+    Outlier_scores = []
+    for i in range(1, 9):
+        infile = open(f'Data_pkl/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_positions.pkl', 'rb')
+        Parsed_positions = pickle.load(infile)
+        infile = open(f'Data_pkl/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_scores.pkl', 'rb')
+        Tajimas_ds = pickle.load(infile)
+        infile.close()
 
-    plt.figure(figsize=(40, 5))
-    plt.plot(Parsed_positions, Tajimas_ds, Color='blue', linewidth=0.5)
-    plt.plot(Sorted_positions, Sorted_scores, 'ro', markersize=2)
-    plt.title('Balancing Selection Candidate Sites')
-    plt.xlabel('Base Pair Position')
-    plt.ylabel("Tajima's D")
-    plt.savefig(f'{args.score_file_name}_figure.png', dpi=500)
+        for j in range(len(Parsed_positions) + 1):
+            if Tajimas_ds[j] >= args.cutt_off:
+                Outlier_positions.append(Parsed_positions[j])
+                Outlier_scores.append(Tajimas_ds[j])
+
+        plt.figure(figsize=(40, 5))
+        plt.plot(Parsed_positions, Tajimas_ds, Color='blue', linewidth=0.5)
+        plt.plot(Outlier_positions, Outlier_scores, 'ro', markersize=2)
+        plt.title('Balancing Selection Candidate Sites')
+        plt.xlabel('Base Pair Position')
+        plt.ylabel("Tajima's D")
+        plt.savefig(f'Figures/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_figure.png', dpi=500)
