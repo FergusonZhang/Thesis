5d06c29b89c27ddc0cea7413c55d3081684683a3
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-28 02:18:21 -0400

    This program will analyze the results from genome parser.

--- code_analyze_the_scores.py
+++ code_analyze_the_scores.py
@@ -1,4 +1,4 @@
-# This program will first determine the cutoff value of top N Tajima's D
+# This program will determine the cutoff Tajima's D value for the whole genome
 # It will then plot the figure of Tajima's D vs. Position for all eight chromosomes
 # Finally, it will return the outliers with corresponding positions for each chromosome
 import argparse
@@ -8,12 +8,12 @@ import pickle
 
 # The main function
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Tajima's D analyzer.")
+    parser = argparse.ArgumentParser(description="Outlier analyzer.")
     parser.add_argument(dest='window_size', help='Please enter the parsing window size.', type=int)
     parser.add_argument(dest='top', help='Please enter the top number.', type=int)
     args = parser.parse_args()
 
-    # Calculate the cutoff value based on all eight chromosomes
+    # Calculate the cutoff value based on the whole genome
     Scores = []
     for i in range(1, 9):
         infile = open(f'Data_pkl/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_scores.pkl', 'rb')
@@ -39,11 +39,11 @@ if __name__ == '__main__':
                 Outlier_scores.append(Tajimas_ds[j])
         print(f'The outliers of chromosome {i} are: ' + str(Outlier_positions))
 
-        # Plot the Tajima's D vs. Position figure
+        # Plot the figure of Tajima's D vs. Position
         plt.figure(figsize=(40, 5))
         plt.plot(Parsed_positions, Tajimas_ds, Color='blue', linewidth=0.5)
         plt.plot(Outlier_positions, Outlier_scores, 'ro', markersize=2)
         plt.title('Balancing Selection Candidate Sites')
         plt.xlabel('Base Pair Position')
         plt.ylabel("Tajima's D")
-        plt.savefig(f'Figures/Cgrand_scaffold_{i}_{args.window_size}_figure.png', dpi=500)
+        plt.savefig(f'Figures/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_figure.png', dpi=500)
