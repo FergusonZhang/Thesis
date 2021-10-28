97d6829dc0c683a068f4167a14bf6e56e6a7eada
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-27 21:20:46 -0400

    Will return the threshold of Top N Tajima's Ds for all eight chromosomes.

---
+++ Pick_the_positions.py
@@ -0,0 +1,22 @@
+# This program will return the cut off value given a window size and an expected number
+import argparse
+import pickle
+
+# The main function
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cut off value determination.')
+    parser.add_argument(dest='window_size', help='Please enter the parsing window size.', type=int)
+    parser.add_argument(dest='expected_number', help='Please enter the expected number.', type=int)
+    args = parser.parse_args()
+
+    Tajimas_ds = []
+    for i in range(1, 9):
+        infile = open(f'Data_pkl/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_scores.pkl', 'rb')
+        Scores = pickle.load(infile)
+        infile.close()
+        Tajimas_ds.append(Scores)
+    Sorted_scores = sorted(Tajimas_ds, reverse=True)
+    Cut_off = Sorted_scores[args.expected_number]
+    print('The window size is: ' + args.window_size)
+    print('The expected number is: ' + args.expected_number)
+    print('The cut off value of all eight chromosomes is: ' + Cut_off)
