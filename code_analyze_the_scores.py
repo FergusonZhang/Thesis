1d12f112858d628c41770c0c763626e2e3b86a2a
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-30 14:14:33 -0400

    This program will analyze the results from genome parser.

--- code_analyze_the_scores.py
+++ code_analyze_the_scores.py
@@ -8,7 +8,7 @@ import pickle
 
 # The main function
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Outlier analyzer.")
+    parser = argparse.ArgumentParser(description='Outlier analyzer.')
     parser.add_argument(dest='window_size', help='Please enter the parsing window size.', type=int)
     parser.add_argument(dest='top', help='Please enter the top number.', type=int)
     args = parser.parse_args()
@@ -21,7 +21,6 @@ if __name__ == '__main__':
         infile.close()
     Sorted_scores = sorted(Scores, reverse=True)
     Cutoff = Sorted_scores[args.top]
-    print('The cutoff value is: ' + str(Cutoff))
 
     # Pick outliers and corresponding positions for each chromosome
     for i in range(1, 9):
@@ -32,12 +31,13 @@ if __name__ == '__main__':
         infile = open(f'Data_pkl/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_scores.pkl', 'rb')
         Tajimas_ds = pickle.load(infile)
         infile.close()
-
         for j in range(len(Parsed_positions)):
             if Tajimas_ds[j] > Cutoff:
                 Outlier_positions.append(Parsed_positions[j])
                 Outlier_scores.append(Tajimas_ds[j])
-        print(f'The outliers of chromosome {i} are: ' + str(Outlier_positions))
+        with open(f'Data_pkl/Cgrand_scaffold_{i}_{args.window_size}_{args.top}_candidates.pkl', 'wb') as p:
+            pickle.dump(Outlier_positions, p)
+        p.close()
 
         # Plot the figure of Tajima's D vs. Position
         plt.figure(figsize=(40, 5))
