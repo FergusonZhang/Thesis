e54c004794bfb2c4d936bf9d0e199ba1689b20a9
zhangz16 <zhangz16@rose-hulman.edu>
2021-10-30 15:04:56 -0400

    This program will take candidate positions and return corresponding gene candidates.

--- code_pick_the_genes.py
+++ code_pick_the_genes.py
@@ -1,4 +1,4 @@
-# This program will take a list of candidate position on a chromosome and return gene candidates
+# This program will take candidate positions and return corresponding gene candidates
 import argparse
 import gffutils
 import pickle
@@ -8,22 +8,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Gene selector.')
     parser.add_argument(dest='window_size', help='Please enter the parsing window size.', type=int)
     parser.add_argument(dest='top', help='Please enter the top number.', type=int)
-    parser.add_argument(dest='scaffold', help='Please enter the scaffold number.', type=int)
     args = parser.parse_args()
-
-    infile = open(
-        f'Data_pkl/Cgrand_scaffold_{args.scaffold}_shapeit4.vcf_{args.window_size}_{args.top}_candidates.pkl', 'rb')
-    Candidates = pickle.load(infile)
-    infile.close()
 
     db = gffutils.create_db('Data_raw/Crubella_474_v1.1.gene.gff3', 'data.db', force=True, keep_order=True,
                             merge_strategy='merge', sort_attribute_values=True)
-    Genes = []
-    for gene in db.features_of_type('gene', order_by='start'):
-        if gene.seqid == f'scaffold_{args.scaffold}':
-            Genes.append(gene)
-    for gene in Genes:
-        for fragment in db.children(gene, order_by='start'):
-            for candidate in Candidates:
-                if fragment.start <= candidate <= fragment.end:
-                    print(fragment.featuretype, fragment.id)
+    for i in range(1, 9):
+        infile = open(
+            f'Data_pkl/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_{args.top}_candidates.pkl', 'rb')
+        Candidates = pickle.load(infile)
+        infile.close()
+        print(f'The gene candidates on chromosome {i} are:')
+
+        for gene in db.features_of_type('gene', order_by='start'):
+            if gene.seqid == f'scaffold_{i}':
+                for candidate in Candidates:
+                    if gene.start <= candidate <= gene.end:
+                        print(gene.featuretype, gene.start, gene.end, gene.id)
