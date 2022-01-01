b4e79d0792140061abd12e02b8a68de444e4bd13
zhangz16 <zhangz16@rose-hulman.edu>
2021-12-31 19:08:46 -0500

    This code is designed to refine the plink map generated by the vcf package.

--- code_change_columns.py
+++ code_change_columns.py
@@ -3,14 +3,15 @@ import pandas as pd
 import numpy as np
 
 if __name__ == '__main__':
-    column_names = ['a', 'b', 'c', 'd']
-    all_data = pd.DataFrame(columns=column_names)
+    # column_names = ['a', 'b', 'c', 'd']
+    # all_data = pd.DataFrame(columns=column_names)
     for i in range(1, 9):
         data = pd.read_csv(f'Data_plink/scaffold_{i}_plink.map', sep='\t', header=None)
         data.columns = ['a', 'b', 'c', 'd']
         data['a'] = [np.int64(i)] * data.shape[0]
         constant = 0.000000001
         data['c'] = constant*data['d']
-        frames = [all_data, data]
-        all_data = pd.concat(frames)
-    all_data.to_csv('Data_plink/modified_scaffold_plink.map', header=None, index=None, sep='\t')
+        data.to_csv(f'Data_plink/modified_scaffold_{i}_plink.map', header=None, index=None, sep='\t')
+        # frames = [all_data, data]
+        # all_data = pd.concat(frames)
+    # all_data.to_csv('Data_plink/modified_scaffold_plink.map', header=None, index=None, sep='\t')
