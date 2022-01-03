7c4729cb9a4a7a279bb33215d41a3a1f26a7d8cf
zhangz16 <zhangz16@rose-hulman.edu>
2022-01-02 19:35:25 -0500

    This code is designed to refine the plink map generated by the vcf package.

--- code_change_columns.py
+++ code_change_columns.py
@@ -26,7 +26,6 @@ if __name__ == '__main__':
         else:
             constant = 1/13362172
         data['c'] = constant*data['d']
-        data['a'].to_csv(f'test_{i}.ehh', header=None, index=None, sep='\t')
         data.to_csv(f'Data_plink/modified_scaffold_{i}_plink.map', header=None, index=None, sep='\t')
         # frames = [all_data, data]
         # all_data = pd.concat(frames)
