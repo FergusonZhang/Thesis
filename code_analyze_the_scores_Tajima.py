3090af0a61164375e867488ad7bf1b5769637001
zhangz16 <zhangz16@rose-hulman.edu>
2022-01-03 23:05:22 -0500

    This program will analyze the results from the genome parser.

--- code_analyze_the_scores_Tajima.py
+++ code_analyze_the_scores_Tajima.py
@@ -1,5 +1,5 @@
 # This program will determine the cutoff Tajima's D value for the whole genome
-# It will then plot the figure of Tajima's D vs. Position for all eight chromosomes
+# It will then plot Tajima's D vs. Position figures for all eight chromosomes
 # Finally, it will return the outliers with corresponding positions for each chromosome
 import argparse
 import matplotlib.pyplot as plt
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     parser.add_argument(dest='top', help='Please enter the top number.', type=int)
     args = parser.parse_args()
 
-    # Calculate the cutoff value based on the whole genome
+    # Determine the cutoff value
     Scores = []
     for i in range(1, 9):
         infile = open(f'Data_pkl/Cgrand_scaffold_{i}_shapeit4.vcf_{args.window_size}_scores.pkl', 'rb')
@@ -39,7 +39,7 @@ if __name__ == '__main__':
             pickle.dump(Outlier_positions, p)
         p.close()
 
-        # Plot the figure of Tajima's D vs. Position
+        # Plot the Tajima's D vs. Position figure
         plt.figure(figsize=(40, 5))
         plt.plot(Parsed_positions, Tajimas_ds, Color='blue', linewidth=0.5)
         plt.plot(Outlier_positions, Outlier_scores, 'ro', markersize=2)
