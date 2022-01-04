2d00c096e38428b5dce81accf6dfd775cacf49f4
zhangz16 <zhangz16@rose-hulman.edu>
2022-01-03 23:58:58 -0500

    This program will analyze the iHH12 scores.

--- code_analyze_the_scores_iHH12.py
+++ code_analyze_the_scores_iHH12.py
@@ -18,5 +18,7 @@ if __name__ == '__main__':
         data = pd.read_csv(f'Results_iHH12/outfile_{i}.ihh12.out', sep='\t', header=None)
         data.columns = ['a', 'b', 'c', 'd']
         Data = pd.concat([Data, data])
+        data.sort_values(by=['d'], ascending=False)
+        print(data.head(10))
     Data.sort_values(by=['d'], ascending=False)
     print(Data)
