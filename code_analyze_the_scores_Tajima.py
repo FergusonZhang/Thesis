95eeaae7503174ea4c7a7fbec543632f02950e70
zhangz16 <zhangz16@rose-hulman.edu>
2022-02-13 14:29:42 -0500

    This program will plot the Tajima's D vs. Position figure as well as return outliers for each chromosome.

--- code_analyze_the_scores_Tajima.py
+++ code_analyze_the_scores_Tajima.py
@@ -35,9 +35,9 @@ if __name__ == '__main__':
         p.close()
 
         # Plot the Tajima's D vs. Position figure
-        plt.figure(figsize=(25, 5))
+        plt.figure(figsize=(15, 5))
         plt.plot(Parsed_positions, Tajimas_ds, 'bo', markersize=0.5)
-        plt.plot(Outlier_positions, Outlier_scores, 'ro', markersize=2)
+        plt.plot(Outlier_positions, Outlier_scores, 'ro', markersize=5)
         plt.title('Balancing Selection Candidate Sites')
         plt.xlabel('Position')
         plt.ylabel("Tajima's D")
