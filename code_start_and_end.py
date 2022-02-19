face4642dbba5af3233de47f8c590cd537a66422
zhangz16 <zhangz16@rose-hulman.edu>
2022-02-19 01:02:47 -0500

    An interesting extra work.

--- code_start_and_end.py
+++ code_start_and_end.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                     if 0 <= (position - gene) <= 25:
                         point = index
                         while Positions[point] <= (gene + 4000):
-                            location =int(Positions[point] - gene)
+                            location = int(Positions[point] - gene)
                             fragment_score[location] = Scores[point]
                             point += 1
                     zipped_lists = zip(Gene_scores, fragment_score)
